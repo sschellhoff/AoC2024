d968b39cb648741edd2ba89e3ee5cdaebdcc785c
sschellhoff <soeren.schellhoff@gmail.com>
2024-12-11 16:17:20 +0100

    refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,12 @@
 package de.sschellhoff
 
-import de.sschellhoff.aoc2024.*
 import de.sschellhoff.utils.Day
+import de.sschellhoff.utils.getDaysForYear
+
 
 fun main() {
-    Day01().run(Day.RunMode.BOTH)
-    Day02().run(Day.RunMode.BOTH)
-    Day03().run(Day.RunMode.BOTH)
-    Day04().run(Day.RunMode.BOTH)
-    Day05().run(Day.RunMode.BOTH)
-    Day06().run(Day.RunMode.BOTH)
-    Day07().run(Day.RunMode.BOTH)
-    Day08().run(Day.RunMode.BOTH)
-    Day09().run(Day.RunMode.BOTH)
-    Day10().run(Day.RunMode.BOTH)
-    Day11().run(Day.RunMode.BOTH)
+    getDaysForYear(2024).forEach { day ->
+        day.run(Day.RunMode.BOTH)
+    }
 }
+
---
+++ src/main/kotlin/aoc2024/Day12.kt
@@ -0,0 +1,13 @@
+package de.sschellhoff.aoc2024
+
+import de.sschellhoff.utils.Day
+
+class Day12: Day(12, 1, 1) {
+    override fun part1(input: String): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: String): Long {
+        TODO("Not yet implemented")
+    }
+}
